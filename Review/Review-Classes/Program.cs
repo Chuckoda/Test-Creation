6999a2d0590df98f9b150dd2428e98cc7a2bb3da
Reid Harrington <Reid.Harrington@ou.edu>
2019-01-30 11:16:32 -0600

    Class stuff

---
+++ Review/Review-Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Reid = new Student();
+            Reid.FirstName = "Reid";
+            Reid.LastName = "Harrington";
+            Reid.SoonerID = 113395107;
+            Reid.IsOnProbation = false;
+            Reid.GPA = 3.72;
+
+            Student Tim = new Student(3, "Timothy", "Fisher", 85000);
+        }
+    }
+}
---
+++ Review/Review-Classes/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Classes
+{
+    class Student
+    {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance;
+
+        /// <summary>
+        /// Default constructor for Student class. Sets default values.
+        /// </summary>
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = "";
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0.0;
+            BursarBalance = 0.0;
+        }
+
+        /// <summary>
+        /// Overloaded constructor for student class that accepts each of the values.
+        /// </summary>
+        /// <param name="id">SoonerID</param>
+        /// <param name="fName">FirstName</param>
+        /// <param name="lName">LastName</param>
+        /// <param name="BursarBalance">BursarBalance</param>
+        public Student (int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            this.BursarBalance = BursarBalance;
+        }
+
+        public void MakePayment(double amount)
+        {
+            if(amount>0 && BursarBalance > amount)
+            {
+                BursarBalance = BursarBalance - amount;
+            }
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+    }
+}
